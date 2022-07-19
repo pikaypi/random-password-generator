0b8eff089d17de7e4ad41a3ae6ee2c36ab7be8c8
Paul Pinard <paul.k.pinard@gmail.com>
2022-07-19 13:57:08 -0400

    prompt for length and validate syntax

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-// Create an object to hold all possible characters
+// An object to hold all possible characters
 var characters = {
     lower: "abcdefghijklmnopqrstuvwxyz",
     upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -8,6 +8,28 @@ var characters = {
     symbols: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 }
 
+// A function to select a random element from a string
+function randomElement(x) {
+  return x[Math.floor(Math.random()*(x.length))];
+}
+
+// A function that takes in user criteria and generates a password
+function generatePassword() {
+    // Prompt user for password length and store as a float
+    var passwordLength = parseFloat(prompt("How long would you like your password to be?", 10));
+
+    // Make sure the length is an integer or restart the function
+    if (passwordLength % 1 !== 0) {
+        alert("Password length must be an integer");
+        generatePassword();
+
+    // Make sure the length is between 8 and 128 or restart the function
+    } else if (passwordLength < 8 || passwordLength > 128) {
+        alert("Passwords must be between 8 and 128 characters");
+        generatePassword();
+    }
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
